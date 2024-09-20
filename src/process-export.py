926184840ce764ec9ec6169df16b64263854eddc
Andreas Timm <info@andreas-timm.dev>
2024-09-20 21:16:56 +0200

    feat: update process-export

--- src/process-export.py
+++ src/process-export.py
@@ -3,11 +3,15 @@ import inspect
 import pathlib
 from dataclasses import dataclass
 
-from src.module.utils import json_load
+from module.utils import json_load
+from signal import signal, SIGPIPE, SIG_DFL
+
+signal(SIGPIPE, SIG_DFL)
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("--input", "-i")
 args = argparser.parse_args()
+
 
 @dataclass
 class FromDict:
@@ -20,9 +24,11 @@ class FromDict:
         # noinspection PyArgumentList
         return cls(**out)
 
+
 @dataclass
 class ExportFromId(FromDict):
     user_id: str
+
 
 @dataclass
 class ExportItem(FromDict):
@@ -30,24 +36,34 @@ class ExportItem(FromDict):
     message: str
     from_id: ExportFromId
 
-data_path = pathlib.Path(args.input)
-data = data_path.open()
 
-counter = 0
-for line in data:
-    data = json_load(line)
-    if 'message' not in data:
-        continue
-    from_id = data.get('from_id')
-    if from_id is not None:
-        user_id = from_id.get('uset_id')
-        if user_id is not None:
-            data['from_id'] = ExportFromId.from_dict(from_id['user_id'])
+def main():
+    data_path = pathlib.Path(args.input)
+    data = data_path.open()
 
-    item = ExportItem.from_dict(data)
-    user = item.from_id.user_id if item.from_id is not None and hasattr(item.from_id, "user_id") else '-'
+    counter = 0
+    for line in data:
+        data = json_load(line)
+        if "message" not in data:
+            continue
+        from_id = data.get("from_id")
+        if from_id is not None:
+            user_id = from_id.get("uset_id")
+            if user_id is not None:
+                data["from_id"] = ExportFromId.from_dict(from_id["user_id"])
 
-    counter += 1
+        item = ExportItem.from_dict(data)
+        user = (
+            item.from_id.user_id
+            if item.from_id is not None and hasattr(item.from_id, "user_id")
+            else "-"
+        )
 
-    line= f"{counter:03} {item.date} {user} {item.message}".replace("\n", "")[:100]
-    print(line)
+        counter += 1
+
+        line = f"{counter:03} {item.date} {user} {item.message}".replace("\n", "")[:100]
+        print(line)
+
+
+if __name__ == "__main__":
+    main()
